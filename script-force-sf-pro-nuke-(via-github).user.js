0aeed5c76751ebda473529a4bd682e9a7a55bc8b
ZΞPHYЯUS <98650152+XYZephyrus@users.noreply.github.com>
2026-09-19 16:15:14 +0800

    Add files via upload

---
+++ script-force-sf-pro-nuke-(via-github).user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Force SF Pro Nuke (via Github)
+// @namespace    via
+// @version      1.2
+// @match        *://*/*
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+    const style = document.createElement("style");
+    style.id = "force-sfpro";
+
+    style.textContent = `
+@font-face {
+    font-family: "MySF";
+    src: url("https://raw.githubusercontent.com/XYZephyrus/SF-Pro-Font-Zeph/main/Roboto-Regular.ttf") format("truetype");
+    font-display: swap;
+}
+
+:root {
+    --fontStack-sansSerif: "MySF" !important;
+    --font-family: "MySF" !important;
+    --font-sans: "MySF" !important;
+    --default-font-family: "MySF" !important;
+}
+
+html,
+body,
+*,
+*::before,
+*::after {
+    font-family: "MySF" !important;
+}
+
+code,
+pre,
+kbd,
+samp {
+    font-family: monospace !important;
+}
+`;
+
+    (document.head || document.documentElement).appendChild(style);
+})();
