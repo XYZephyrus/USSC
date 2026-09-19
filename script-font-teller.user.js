cd5500ba148441a1147d31a0021877d81f3f7da9
ZΞPHYЯUS <98650152+XYZephyrus@users.noreply.github.com>
2026-09-19 16:13:19 +0800

    Add files via upload

---
+++ script-font-teller.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Font Teller
+// @namespace    https://viayoo.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @run-at       document-end
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+
+alert(getComputedStyle(document.documentElement).fontFamily);
