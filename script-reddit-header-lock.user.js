c5cef27a13ffb6aae81f59d4d448acd71f2d8613
ZΞPHYЯUS <98650152+XYZephyrus@users.noreply.github.com>
2026-09-20 21:47:48 +0800

    Create script-reddit-header-lock.user.js

---
+++ script-reddit-header-lock.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name                  Reddit Header Lock
+// @namespace       NS
+// @version               v1
+// @description        TBA
+// @author                Zephyrus
+// @run-at                 document-start
+// @match           
+// @grant                  GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle(`
+
+.end-0 {
+ position: fixed !important;
+ }
+    
+    `);
+    OR
+    });
+})();
